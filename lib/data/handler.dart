f3ef07d0f8c8c12f8a73f902d3f0189f82bfccc6
hbenpitsu <hbenpitsu15@gmail.com>
2024-10-17 09:35:50 +0900

    inp

--- lib/data/handler.dart
+++ lib/data/handler.dart
@@ -2,9 +2,6 @@ import 'package:miraibo/data/objects.dart';
 
 import '../page/scheduling_page.dart';
 
-/* 
-This fetches lists of ticket config data.
-*/
 class TicketDataManager {
   TicketDataManager._internal();
 
@@ -12,34 +9,15 @@ class TicketDataManager {
   static final TicketDataManager _instance = TicketDataManager._internal();
   factory TicketDataManager() => _instance;
 
-  /*
-  This is called by TicketContainer of DailyScreen in scheduling_page.
-  This returns the list of tickets that belong to the given date.
-  */
+  /// Returns ticket configs for the given [date].
   Future<List<TicketConfigData>> fetchTicketConfigsFor(DateTime date) async {
     return [];
   }
 
-  /*
-  This is called by TicketContainer of TicketPage.
-  Notable tickets are followings:
-  - Display Tickets
-  - Schedule Tickets whose schedule is today or before today
-  - Recent Log Tickets
-  */
   Future<List<TicketConfigData>> fetchNotableTicketConfigs() async {
     return [];
   }
 
-  /* This method is invoked by `LogTicketConfigSectionWithPreset`.
-  
-  Its primary purpose is to fetch and return a list of `preset`s for log tickets.
-  The number of presets to be fetched is specified by `nPresets`.
-  
-  `Preset`s are instances of `LogTicketConfigData`. When a preset is applied to a `logTicketConfigData`,
-  certain necessary fields are copied from the preset, while other fields are ignored.
-  The functionality for applying presets is implemented in `objects.dart` within the `LogTicketConfigData` class.
-  */ 
   Future<List<LogTicketConfigData>> fetchLogTicketPresets(int nPresets) async {
     return List.generate(
         nPresets,
@@ -53,8 +31,7 @@ class TicketDataManager {
   }
 
   /* 
-  The style of a date button (on monthly_calendar in monthly_screen in scheduling_page) varies 
-  depending on existance of tickets that belong to the date.
+  The style of a date button (on monthly_calendar in monthly_screen in scheduling_page) varies depending on existance of tickets that belong to the date.
   This method calculates the style of the date button for the given date consulting the data store.
   */
   Future<DateButtonStyle> calcStyleForDateButton(DateTime date) async {
@@ -62,9 +39,6 @@ class TicketDataManager {
   }
 }
 
-/*
-Consulting multiple data sources, this class calculate the number to be displayed.
-*/
 class StatisticalAnalyzer {
   StatisticalAnalyzer._internal();
 
